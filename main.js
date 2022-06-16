6cb1f9da9410b963100469852341f43cbb7808ee
RAFAEL MORAES <71736439+rflfragamoraes@users.noreply.github.com>
2022-06-16 18:54:27 -0300

    Projeto Biblioteca V 2.0

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const retiradaLivro = require('./retirada_livro.js')
 //******************************************************************************************************
 
 
-/*cadastroAluno.inserirAluno({matricula: 777, nome_aluno: "Joao", curso:"Medicina"},
+/*cadastroAluno.inserirAluno({matricula: 333, nome_aluno: "Debora", curso:"ADS"},
     function(err, alunoInserido) {
         console.log("INSERINDO Aluno...")
         if(err) {
@@ -19,12 +19,10 @@ const retiradaLivro = require('./retirada_livro.js')
             console.log("Aluno inserido: ");
             console.log(alunoInserido);
         }
-    });
+    });*/
 
 
-
-
-cadastroAluno.listarAluno(
+/*cadastroAluno.listarAluno(
     function(err, aluno) {
         console.log("Listar: ");
         if(err) {
@@ -36,10 +34,10 @@ cadastroAluno.listarAluno(
 
         }
     }
-);
+);*/
 
 
-cadastroAluno.deletarAluno(555, function(erro, aluno) {
+/*cadastroAluno.deletarAluno(555, function(erro, aluno) {
     console.log("Aluno deletado (2): ");
     if(erro) {
         console.log("Erro: "+erro);
@@ -47,10 +45,10 @@ cadastroAluno.deletarAluno(555, function(erro, aluno) {
     else {
         console.log(aluno);
     }
-});
+});*/
 
 
-cadastroAluno.atualizarAluno({matricula: 111, nome_aluno: "Rafael Moraes", curso: "Redes"}, 
+/*cadastroAluno.atualizarAluno({matricula: 111, nome_aluno: "Rafael Moraes", curso: "ADS"}, 
     function(erro, aluno) {
         console.log("Atualizar Aluno(001): ");
         if(erro) {
@@ -59,10 +57,9 @@ cadastroAluno.atualizarAluno({matricula: 111, nome_aluno: "Rafael Moraes", curso
         else {
             console.log(aluno);
         }
-});
+});*/
 
-
-cadastroAluno.buscarAluno(matricula = 111, 
+/*cadastroAluno.buscarAluno(matricula = 111, 
     function(erro, aluno) {
         console.log("BuscarAluno: ");
         if(erro) {
@@ -91,10 +88,10 @@ cadastroAluno.buscarAluno(matricula = 111,
             console.log("Livro inserido: ");
             console.log(livroInserido);
         }
-    });
+    });*/
 
 
-cadastroLivro.listarLivro(
+/*cadastroLivro.listarLivro(
     function(err, livros) {
         console.log("Listar Livro: ");
         if(err) {
@@ -106,10 +103,10 @@ cadastroLivro.listarLivro(
 
         }
     }
-);
+);*/
 
 
-cadastroLivro.deletarLivro(4444, function(erro, livro) {
+/*cadastroLivro.deletarLivro(4444, function(erro, livro) {
     console.log("Livro deletado com sucesso... ");
     if(erro) {
         console.log("Erro: "+erro);
@@ -120,7 +117,7 @@ cadastroLivro.deletarLivro(4444, function(erro, livro) {
 });*/
 
 
-cadastroLivro.atualizarLivro({ codigo:8888, nome_livro: "Perdido no espaco", autor: "Carlos Miguel", quantidade: 6}, 
+/*cadastroLivro.atualizarLivro({ codigo:8888, nome_livro: "Perdido no espaco", autor: "Carlos Miguel", quantidade: 6}, 
     function(erro, livro) {
         console.log("Atualizar Livro: ");
         if(erro) {
@@ -129,7 +126,7 @@ cadastroLivro.atualizarLivro({ codigo:8888, nome_livro: "Perdido no espaco", aut
         else {
             console.log(livro);
         }
-});
+});*/
 
 
 /*cadastroLivro.buscarLivro(8888, 
@@ -149,8 +146,64 @@ cadastroLivro.atualizarLivro({ codigo:8888, nome_livro: "Perdido no espaco", aut
 //Retirada Livro
 //******************************************************************************************************
 
-retiradaLivro.retirarLivro();
+// funcao para consultar aluno, consular livro e se existente fazer o cadastro da retirada
+/*retiradaLivro.consultaAluno(nome_aluno = "Debora", 
+    function(erro, item1) {
+        console.log("Aluno: ");
+        if(erro) {
+            console.log("Erro: "+erro);
+        }
+        else {
+            console.log(item1);    
 
-retiradaLivro.entregarLivro();
+            retiradaLivro.consultaLivro(nome_livro = "H", 
+            function(erro, item2) {
+                console.log("Livro: ");
+                if(erro) {
+                    console.log("Erro: "+erro);
+                }
+                else {
+                    console.log(item2);  
 
-retiradaLivro.situacaoLivro();
+                    retiradaLivro.retirarLivro({nome_aluno: item1, nome_livro: item2, data_retirada: "16/06", data_entrega: "21/06"},
+                    function(err, retirar) {
+                        console.log("Resgistando retirada...")
+                        if(err) {
+                            console.log("Aconteceu algum erro... tente novamente");
+                            console.log(err);
+                        }
+                        else {
+                            console.log("Livro retirado com sucesso...");
+                            console.log(retirar);
+                        }
+                    });       
+
+            }
+        });        
+
+    }
+});*/
+
+
+
+/*retiradaLivro.entregarLivro("Debora", "H", function(erro, entregar) {
+    console.log("Entregando o Livro... ");
+    if(erro) {
+        console.log("Erro: "+erro);
+    }
+    else {
+        console.log(entregar);
+    }
+});*/
+
+
+/*retiradaLivro.situacaoLivro("H2", 
+    function(erro, livro) {
+        console.log("Consultando Livro: ");
+        if(erro) {
+            console.log("Erro: "+erro);
+        }
+        else {
+            console.log("Este livro " +livro+ " nao esta disponivel para retirada");
+        }
+});*/
--- retirada_livro.js
+++ retirada_livro.js
@@ -8,13 +8,125 @@ const conexao = {
     database: 'crud_biblioteca'
 };
 
-function retirarLivro() {}
 
-function entregarLivro() {}
+function consultaAluno(nome_aluno, callback){
+    const cliente = new Client(conexao);
+    cliente.connect();
+    
+    const sql = "SELECT * FROM tabela_aluno WHERE nome_aluno=$1";
+    const values = [nome_aluno];
 
-function situacaoLivro() {}
+    cliente.query(sql, values,
+        function (err, res) {
+            if(err) {
+                callback(err.message, undefined);                
+            }
+            else if (res.rows && res.rows.length > 0) {
+                let aluno1 = nome_aluno;
+                callback(undefined, aluno1);
+            }
+            else {
+                const error = "Aluno nao encontrado";
+                callback(error, undefined);
+            }
+
+            cliente.end();
+        }
+    )    
+}
+
+function consultaLivro(nome_livro, callback){
+    const cliente = new Client(conexao);
+    cliente.connect();
+    
+    const sql = "SELECT * FROM tabela_livro WHERE nome_livro=$1";
+    const values = [nome_livro];
+
+    cliente.query(sql, values,
+        function (err, res) {
+            if(err) {
+                callback(err.message, undefined);                
+            }
+            else if (res.rows && res.rows.length > 0) {
+                let aluno2 = nome_livro;
+                callback(undefined, aluno2);
+            }
+            else {
+                const error = "Livro nao encontrado";
+                callback(error, undefined);
+            }
+
+            cliente.end();
+        }
+    )    
+}
+
+function retirarLivro(alunoLivro, callback) {
+    const cliente = new Client(conexao);
+    cliente.connect();
+
+    const sql = "INSERT INTO tabela_retirada (nome_aluno, nome_livro, data_retirada, data_entrega) VALUES ($1, $2, $3, $4) RETURNING *";
+    const values = [alunoLivro.nome_aluno, alunoLivro.nome_livro, alunoLivro.data_retirada, alunoLivro.data_entrega];
+
+    cliente.query(sql, values, 
+        function (err, res){
+            callback(err, res.rows[0]);
+            cliente.end();
+        })
+
+}
+
+function entregarLivro(nome_aluno, nome_livro, callback) {
+    const cliente = new Client(conexao);
+    cliente.connect();
+
+    const sql = "DELETE FROM tabela_retirada WHERE nome_aluno=$1 AND nome_livro=$2 RETURNING *"
+    const values = [nome_aluno, nome_livro];
+
+    cliente.query(sql, values, function(err, res) {
+        if(err) {
+            callback(err.message, undefined);                
+        }
+        else if (res.rows && res.rows.length > 0) {
+            let aluno = res.rows[0];
+            callback(undefined, aluno);
+        }
+        else {
+            const error = "Aluno ou Livro nao registrado...";
+            callback(error, undefined);
+        }
+
+        cliente.end();        
+    })
+}
+
+function situacaoLivro(nome_livro, callback){
+    const cliente = new Client(conexao);
+    cliente.connect();
+    
+    const sql = "SELECT * FROM tabela_retirada WHERE nome_livro=$1";
+    const values = [nome_livro];
+
+    cliente.query(sql, values,
+        function (err, res) {
+            if(err) {
+                callback(err.message, undefined);                
+            }
+            else if (res.rows && res.rows.length > 0) {
+                
+                callback(undefined, nome_livro);
+            }
+            else {
+                const error = "O livro " +nome_livro+ " esta disponivel para retirada";
+                callback(error, undefined);
+            }
+
+            cliente.end();
+        }
+    )    
+}
 
 
 module.exports = {
-    retirarLivro, entregarLivro, situacaoLivro
+    consultaAluno, consultaLivro, retirarLivro, entregarLivro, situacaoLivro
 }
